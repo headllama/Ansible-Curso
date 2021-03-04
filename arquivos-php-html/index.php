6ef1aab555a494a97746d975987b9b81b3d80eb6
walber.silva <walber.silva@csi.ind.br>
2021-03-04 10:45:27 -0300

    Adicionando arquivos-php-html

---
+++ arquivos-php-html/index.php
@@ -0,0 +1,11 @@
+<?php
+  $login_cookie = $_COOKIE['login'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      echo"Essas informacoes <font color='red'>PODEM</font> ser acessadas por voce";
+    }else{
+      echo"Bem-Vindo, convidado <br>";
+      echo"Essas informacoes <font color='red'>NÃO PODEM</font> ser acessadas por voce";
+      echo"<br><a href='login.html'>Faca Login</a> Para ler o contudo";
+    }
+?>
---
+++ arquivos-php-html/login.php
@@ -0,0 +1,21 @@
+<?php
+$login = $_POST['login'];
+$entrar = $_POST['entrar'];
+$senha = md5($_POST['senha']);
+$connect = mysql_connect('SERVER_NAME','USER_NAME','PASSWD');
+$db = mysql_select_db('DB_NAME');
+  if (isset($entrar)) {
+
+    $verifica = mysql_query("SELECT * FROM usuarios WHERE login =
+    '$login' AND senha = '$senha'") or die("erro ao selecionar");
+      if (mysql_num_rows($verifica)<=0){
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='login.html';</script>";
+        die();
+      }else{
+        setcookie("login",$login);
+        header("Location:index.php");
+      }
+  }
+?>
